e8dec1d72651f0e01758d48f73be44b2ccc7a645
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2014-06-20 18:50:45 +0000

    New version of THBusiness - 1.0.1
    git-svn-id: https://themes.svn.wordpress.org/thbusiness@36173 8c925393-aaf8-4f5c-984e-1f1965effe94

---
+++ 1.0.1/sidebar-footer-mid.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package thmag
+ */
+?>
+	<div id="secondary" class="widget-area" role="complementary">
+		<?php if ( ! dynamic_sidebar( 'footer-mid' ) ) : ?>
+
+
+		<?php endif; // end sidebar widget area ?>
+	</div><!-- #secondary -->
