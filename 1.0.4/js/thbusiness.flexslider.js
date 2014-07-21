36e0c646441839d8e950d5e0454173bca01d9e30
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2014-07-21 18:56:38 +0000

    New version of THBusiness - 1.0.4
    git-svn-id: https://themes.svn.wordpress.org/thbusiness@37066 8c925393-aaf8-4f5c-984e-1f1965effe94

---
+++ 1.0.4/js/thbusiness.flexslider.js
@@ -0,0 +1,8 @@
+jQuery(window).load(function() {
+  jQuery('.flexslider').flexslider({
+    animation: "slide",
+    direction: "horizontal",
+	slideshowSpeed: 7000,
+	animationSpeed: 600
+  });
+});
